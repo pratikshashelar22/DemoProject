106ade0d7f913d1b105c656ea10bafd78196dff2
pratiksha <pratikshashelar22@gmail.com>
2019-09-16 17:25:40 +0530

    project ui

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -1,5 +1,10 @@
 angular.module('app')
 .controller('homectlr',function($scope,$state){
+	$round = true;
+	$scope.ChangeContent = function(){
+		angular.element(document.getElementById("p1")).text("Value changed");
+		angular.element( $("ion-navicon-round").toggle("ion-navicon-cross"));
+	};
 
 })
 .controller('menuctlr', function($scope, $stateParams) {
