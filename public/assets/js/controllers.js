b5409421940ee16cfeeb35deb9bca97e4a919b32
pratiksha <pratikshashelar22@gmail.com>
2019-09-18 18:40:01 +0530

    Menu select scroll

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -1,9 +1,21 @@
 angular.module('app')
-.controller('homectlr',function($scope,$state){
+.controller('homectlr', function($anchorScroll,$location, $scope,$state){
 	$round = true;
 	$scope.ChangeContent = function(){
 		angular.element(document.getElementById("p1")).text("Value changed");
 		angular.element( $("ion-navicon-round").toggle("ion-navicon-cross"));
+	};
+	$scope.gotoAnchor = function(section){
+		var newHash = section;
+	      if ($location.hash() !== newHash) {
+	        // set the $location.hash to `newHash` and
+	        // $anchorScroll will automatically scroll to it
+	        $location.hash(section);
+	      } else {
+	        // call $anchorScroll() explicitly,
+	        // since $location.hash hasn't changed
+	        $anchorScroll();
+   		}
 	};
 
 })
