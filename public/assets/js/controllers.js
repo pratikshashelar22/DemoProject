235ed67e715570f5cf3cb1cefd49ba31ff162554
pratiksha <pratikshashelar22@gmail.com>
2019-10-04 18:02:13 +0530

    ui

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -54,12 +54,49 @@ angular.module('app')
 	}
 	 $scope.openModal = function() {
       $scope.modal.show();
-   };
+  	};
 	
-   $scope.closeModal = function() {
-      $scope.modal.hide();
-   };
-
+   	$scope.closeModal = function() {
+      	$scope.modal.hide();
+  	};
+   	$('#recipeCarousel').carousel({
+	  interval: 10000
+	})
+   	$('#recipeCarousel2').carousel({
+	  interval: 10000
+	})
+	$('.carousel .carousel-item').each(function(){
+	    var next = $(this).next();
+	    if (!next.length) {
+	    next = $(this).siblings(':first');
+	    }
+	    next.children(':first-child').clone().appendTo($(this));
+	    
+	    for (var i=0;i<2;i++) {
+	        next=next.next();
+	        if (!next.length) {
+	        	next = $(this).siblings(':first');
+	      	}
+	        
+	        next.children(':first-child').clone().appendTo($(this));
+	      }
+	});
+	$('.carousel .carousel-item2').each(function(){
+	    var next = $(this).next();
+	    if (!next.length) {
+	    next = $(this).siblings(':first');
+	    }
+	    next.children(':first-child').clone().appendTo($(this));
+	    
+	    for (var i=0;i<2;i++) {
+	        next=next.next();
+	        if (!next.length) {
+	        	next = $(this).siblings(':first');
+	      	}
+	        
+	        next.children(':first-child').clone().appendTo($(this));
+	      }
+	});
 })
 .controller('menuctlr', function($scope, $stateParams) {
    
